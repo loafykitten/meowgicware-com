e486f04d43f4adb62915dce40050c942ddb0a7cb
loafykitten <hello@loafykitten.cafe>
2025-07-05 18:30:36 -0500

    feat: site content updates

--- ClairWeb/src/app/layout.tsx
+++ ClairWeb/src/app/layout.tsx
@@ -4,6 +4,14 @@ import '@/styles/global.css'
 import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
 
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+library.add(fab)
+library.add(fas)
+
 const dmSans = DM_Sans({
     variable: '--font-dm-sans',
     subsets: ['latin'],
