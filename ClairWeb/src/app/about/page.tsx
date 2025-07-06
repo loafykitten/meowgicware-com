b90510267b4bd4c9ba473cf31f0bb575a2a953fa
loafykitten <hello@loafykitten.cafe>
2025-07-05 21:51:29 -0500

    feat: added about page skeleton

---
+++ ClairWeb/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import TexturedHeader from '@/components/textured-header';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <TexturedHeader title="Magic Behind Meowgic" />
+      {/* Future content for the About page will go here */}
+    </div>
+  );
+}
